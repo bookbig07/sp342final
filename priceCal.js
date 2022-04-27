a93cb5126b3674c24a844dfdebf425fe2b0a1f32
Bookbig <Bookbig@gmail.com>
2022-04-27 14:20:09 +0700

    Fix Bug #4 : Fix to show popup message

--- priceCal.js
+++ priceCal.js
@@ -1,9 +1,11 @@
+const  InDexValuex = [ 91.25 , 79.88, 83.12, 100 ]
+
 function priceCalculate() {
   let ticknum = (document.getElementById("ticknum").value).trim();
   let selectObj = document.getElementById("event");
   let eventId = parseInt(selectObj.options[selectObj.selectedIndex].value);
-  
-  return 0;
+  let Mathx = ( InDexValuex[eventId] * ticknum )
+  return Mathx;
 }
 
 
